52adcfb7e74aac6a6afa2d404c0ba2e4612db400
PROG WEB - MANHA <PROG WEB - MANHA@MININT-QVG78E3>
2021-01-14 12:55:02 -0200

    correção das pastas para funcionar o bootstrap

--- paginas/include/footer.php
+++ paginas/include/footer.php
@@ -9,6 +9,8 @@
 
 
 
-<script src="../../plugins/bootstrap/js/bootstrap.min.js" ></script>
+<script src="plugins/js/jquery-3.5.1.min.js" ></script>
+<script src="plugins/bootstrap/js/bootstrap.min.js" ></script>
+<script src="plugins/js/inicial.js" ></script>
 </body>
 </html>
--- paginas/include/header.php
+++ paginas/include/header.php
@@ -2,7 +2,7 @@
     <head>
         <meta charset="UTF-8">
         <link rel="stylesheet" href="plugins/bootstrap/css/bootstrap.min.css"/>
-        <link rel="stylesheet" href=""/>
+        <link rel="stylesheet" href="plugins/css/inicial.css"/>
         <title>Blog Iniciantes</title>
     </head> 
     <header>
