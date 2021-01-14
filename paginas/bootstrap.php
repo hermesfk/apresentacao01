6d70a8cac8d6567bd32e8e7576ce646f4a1d9964
55819 <55819@10.0.0.104>
2021-01-14 16:23:34 -0300

    ultima att pasta bootstrap

--- paginas/bootstrap.php
+++ paginas/bootstrap.php
@@ -1,17 +1,37 @@
 <div class="container text-center">
     <div class="row">
         <div class="col">
-            <h1> Como surgiu o Bootstrap?</h1>
+            <h1> Como surgiu o <a href="https://getbootstrap.com.br/">Bootstrap?</a></h1>
             <img src="plugins/img/bootstrap-bom-ou-ruim.jpg" alt="" />
             <p class="my-3">
-                Ele foi desenvolvido por Jacob Thorton e Mark Otto, engenheiros do Twitter, como uma tentativa de resolver incompatibilidades dentro da própria equipe. O intuito era otimizar o desenvolvimento de sua plataforma através da adoção de uma estrutura única. Isto reduziria inconsistências entre as diversas formas de se codificar, que variam de profissional para profissional. E a tentativa deu tão certo que eles perceberam o grande potencial da ferramenta, lançando-a no GitHub como um software livre.
+                Ele foi desenvolvido por Jacob Thorton e Mark Otto, engenheiros do Twitter, como uma tentativa de resolver incompatibilidades dentro da própria equipe. O intuito era otimizar o desenvolvimento de sua plataforma através da adoção de uma estrutura única. Isto reduziria inconsistências entre as diversas formas de se codificar, que variam de profissional para profissional. E a tentativa deu tão certo que eles perceberam o grande potencial da ferramenta, lançando-a no <a href="https://github.com/" > GitHub </a> como um software livre.
             </p>
 
             <h1>Como usar o Bootstrap?</h1>
-            <img src="plugins/img/1_TJT7z7w3baYO0ON-6RbaYA.png" alt="" />
-            
-            <p class="my-3">
-                É bem simples: faça o download do Bootstrap no site oficial da ferramenta. Adicione os arquivos “bootstrap.min.css” e “bootstrap.min.js” nas pastas correspondentes a eles dentro do seu site. Depois, é só chamar o CSS e o JS dentro da tag <head> do HTML. É importante lembrar que o Bootstrap acessa a biblioteca jQuery para ativar o comportamento de seus plugins. Portanto, lembre-se de inseri-lo em sua página para que tudo funcione perfeitamente.
+            <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img class="d-block w-100" src="plugins/img/1_TJT7z7w3baYO0ON-6RbaYA.png" alt="Primeiro Slide">
+                    </div>
+                    <div class="carousel-item">
+                        <img class="d-block w-100" src="plugins/img/bootstrap-bom-ou-ruim.jpg" alt="Segundo Slide">
+                    </div>
+                    <div class="carousel-item">
+                        <img class="d-block w-100" src="plugins/img/curso_o-que-e-bootstrap_1961.jpg" alt="Terceiro Slide">
+                    </div>
+                </div>
+                <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Anterior</span>
+                </a>
+                <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Próximo</span>
+                </a>
+            </div>
+
+            <p class="my-3 ">
+                É bem simples: faça o<a href="https://getbootstrap.com.br/docs/4.1/getting-started/download/" > download</a> do Bootstrap no site oficial da ferramenta. Adicione os arquivos “bootstrap.min.css” e “bootstrap.min.js” nas pastas correspondentes a eles dentro do seu site. Depois, é só chamar o CSS e o JS dentro da tag <head> do HTML. É importante lembrar que o Bootstrap acessa a biblioteca jQuery para ativar o comportamento de seus plugins. Portanto, lembre-se de inseri-lo em sua página para que tudo funcione perfeitamente.
                 Para facilitar o seu entendimento sobre o Bootstrap, há no site uma área onde você consegue ver exemplos dos recursos disponíveis para você customizar o seu site. Para isto, basta acessar a Coleção de Exemplos. Lá, você encontra modelos de temas, layouts de tabelas, barras de navegação, etc.
                 Além disto, existe também uma sessão onde você pode ver alguns belos, criativos e funcionais projetos já realizados com o Bootstrap. Entre em Explorar Expo e inspire-se. Agora é só mergulhar nas infinitas opções que o Bootstrap oferece e deixar sua criatividade fluir, criando projetos com muito mais praticidade e em muito menos tempo.    
                 </p>
