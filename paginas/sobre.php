8d979aebae11903d005503f55caae4b838ac7531
SemiramisCavalcanti <webdesigncavalcanti@gmail.com>
2021-01-17 15:11:11 -0300

    Finlizado a pagina sobre !!!!

--- paginas/sobre.php
+++ paginas/sobre.php
@@ -1,102 +1,289 @@
 <link rel="stylesheet" href="plugins/css/sobre.css"/>
 
 <div id="sobre" class="contaner">
+
+    <div class="textosobre">
+        
+        <h3 class="ml-3">Sobre</h3>
+        <br>            
+        <h5 class="ml-5 mr-2 text-justify">Quando nos foi pedido para criar um site, agregamos nossa problemática de iniciantes na área de programação 
+            e em conjunto decidimos criar um site, estilo blog, contando nossas experiências com as ferramentas utilizadas 
+            neste ramo, e também mostrar com linguagem mas de iniciantes. Mostrando algumas dificuldades que encontramos, e 
+            como solucionamos, trazer novidades do mercado e como estamos buscando ingressar na profissão. 
+        </h5>
+       
+
+    </div>
+
+
     <div class=" row">
         <div class="col">
-            
 
-    <section id="aboutPage">
-        
-            <div class="aboutMe">
-                <article class="aboutMeContainer card-front">
-                    <header class="aboutHeader">
-                        <img class="headerProfile img-fluid" src="../plugins/img/post01.jpg" alt="header image" />
-                        <h1 class="fadeInLeft animated">Felipe Martinin</h1>
-                        <p class="fadeInLeft animated">UI/ UX Designer - Web Developer</p>
-                    </header>
 
-                    <div class="frontContent">
-                        <p>Tenho 21 anos, moro na Grande São Paulo e sou 
-                            apaixonado pela Web. Ela me cativa principalmente
-                            por sua grande colaboração com o desenvolvimento
-                            social. Ela é um novo mundo capaz de inserir a 
-                            nossa sociedade moderna a um novo patamar 
-                            jamais visto antes. E meu maior objetivo é 
-                            contribuir notoriamente com essa evolução </p>
+            <section id="aboutPage">
 
-                    </div>
-                    <div class="socialIcons">
-                        <a href=""><i class="icon-facebook"></i></i></a>
-                        <a href=""><i class="icon-twitter"></i></a>
-                        <a href=""><i class="fa fa-google-plus-square"></i></a>
-                        <a href=""><i class="icon-github"></i></a>
-                        <a href=""><i class="fa fa-youtube"></i></a>
-                        <a href=""><i class="fa fa-dribbble"></i></a>
-                        <a href=""><i class="fa fa-linkedin-square last-icon"></i></i></a>
-                    </div>
-                    <footer class="aboutFooter">
-                        <a href="" class="goContact">Contato</a>
-                        <a class="goFlip">Currículo</a>
-                    </footer>
-                </article>
+                <div class="aboutMe">
+                    <article class="aboutMeContainer card-front">
+                        <header class="aboutHeader">
+                            <img class="headerProfile img-fluid" src="../plugins/img/post01.jpg" alt="header image" />
+                            <h1 class="fadeInLeft animated">Petterson</h1>
+                            <p class="fadeInLeft animated">UI/ UX Designer - Web Developer</p>
+                        </header>
 
-                <!-- card back -->
+                        <div class="frontContent">
+                            <p>Tenho 21 anos, moro na Grande São Paulo e sou 
+                                apaixonado pela Web. Ela me cativa principalmente
+                                por sua grande colaboração com o desenvolvimento
+                                social. Ela é um novo mundo capaz de inserir a 
+                                nossa sociedade moderna a um novo patamar 
+                                jamais visto antes. E meu maior objetivo é 
+                                contribuir notoriamente com essa evolução </p>
 
-                <div class="aboutMeContainer card-back">
-
-                    <header class="aboutHeader">
-                        <h2 class="animated">Currículo</h2>
-                    </header>
-
-                    <div class="backContent-wrap">
-
-                        <div class="backContent animated">
-                            <p><b>Resumo: </b>  Sou desenvolvedor front-end e designer de interfaces. E um grande fanático por usabilidade e por experiências visuais únicas. Gosto de trabalhar com desenvolvimento Web pois ela abrange vários campos de conhecimentos. 
-                                <br />
-                                <br />
-                                Isso permite que eu não seja um profissional que só interage apenas com códigos. Por isso além de estudar unicamente tecnologias ligadas à web. Também sou um grande estudante de Direito, Psicologia, Marketing e User Experience.
-                            </p>
-
-                            <p><b>Formação Acadêmica: </b> Cursando o penúltimo
-                                semestre do curso Sistemas para Internet na
-                                Universidade Municipal de São Caetano do
-                                sul (USCS).
-                            </p>
-                            <p class="list">
-                                <b>Conhecimentos: </b>
-                                <br />
-                                <br />
-                                • HTML5, CSS3. JavaScript e jQuery<br />
-                                • Linguagens para dados estruturados:  Microdata,
-                                Microformats e RDFa.<br />
-                                • WAI-ARIA para acessibilidade <br />
-                                • CMS’s: Wordpress e Magento<br />
-                                • UX/UI Design<br />
-                                • Marketing e Publicidade digital<br />
-                                • SEO, SEM e SMM<br />
-                                • Scrum e Kanban para gerenciamento de projetos <br />
-                                • Git para versionamento de projetos <br />
-                                • Inglês técnico <br />
-                            </p>
                         </div>
-                    </div>
+                        <div class="socialIcons">
+                            <a href=""><i class="icon-facebook"></i></i></a>
+                            <a href=""><i class="icon-twitter"></i></a>
+                            <a href=""><i class="icon-gmail"></i></a>
+                            <a href=""><i class="icon-github"></i></a>
+                            <a href=""><i class="icon-youtube"></i></a>
+                            <a href=""><i class="icon-instagram"></i></a>
+                            <a href=""><i class="icon-linkedin"></i></i></a>
+                        </div>
+                        <footer class="aboutFooter">
+                            <a href="" class="goContact">Contato</a>
+                            <a class="goFlip">Portifólio</a>
+                        </footer>
+                    </article>
 
-                    <footer class="aboutFooter">
-                        <a href="" class="goContact">Contato</a>
-                        <a class="goFlip">Voltar</a>
-                    </footer>
+                    <!-- card back -->
+
+                    <div class="aboutMeContainer card-back">
+
+                        <header class="aboutHeader">
+                            <h2 class="animated">Currículo</h2>
+                        </header>
+
+                        <div class="backContent-wrap">
+
+                            <div class="backContent animated">
+                                <p><b>Resumo: </b>  Sou desenvolvedor front-end e designer de interfaces. E um grande fanático por usabilidade e por experiências visuais únicas. Gosto de trabalhar com desenvolvimento Web pois ela abrange vários campos de conhecimentos. 
+                                    <br />
+                                    <br />
+                                    Isso permite que eu não seja um profissional que só interage apenas com códigos. Por isso além de estudar unicamente tecnologias ligadas à web. Também sou um grande estudante de Direito, Psicologia, Marketing e User Experience.
+                                </p>
+
+                                <p><b>Formação Acadêmica: </b> Cursando o penúltimo
+                                    semestre do curso Sistemas para Internet na
+                                    Universidade Municipal de São Caetano do
+                                    sul (USCS).
+                                </p>
+                                <p class="list">
+                                    <b>Conhecimentos: </b>
+                                    <br />
+                                    <br />
+                                    • HTML5, CSS3. JavaScript e jQuery<br />
+                                    • Linguagens para dados estruturados:  Microdata,
+                                    Microformats e RDFa.<br />
+                                    • WAI-ARIA para acessibilidade <br />
+                                    • CMS’s: Wordpress e Magento<br />
+                                    • UX/UI Design<br />
+                                    • Marketing e Publicidade digital<br />
+                                    • SEO, SEM e SMM<br />
+                                    • Scrum e Kanban para gerenciamento de projetos <br />
+                                    • Git para versionamento de projetos <br />
+                                    • Inglês técnico <br />
+                                </p>
+                            </div>
+                        </div>
+
+                        <footer class="aboutFooter">
+                            <a href="" class="goContact">Contato</a>
+                            <a class="goFlip">Voltar</a>
+                        </footer>
+                    </div>
                 </div>
-            </div>
-    </section>
+            </section>
         </div>
         <div class="col">
-            
+
+            <section id="aboutPage">
+
+                <div class="aboutMe">
+                    <article class="aboutMeContainer card-front">
+                        <header class="aboutHeader">
+                            <img class="headerProfile img-fluid" src="../plugins/img/post02.jpg" alt="header image" />
+                            <h1 class="fadeInLeft animated">Felicity</h1>
+                            <p class="fadeInLeft animated">UI/ UX Designer - Web Developer</p>
+                        </header>
+
+                        <div class="frontContent">
+                            <p>Tenho 21 anos, moro na Grande São Paulo e sou 
+                                apaixonado pela Web. Ela me cativa principalmente
+                                por sua grande colaboração com o desenvolvimento
+                                social. Ela é um novo mundo capaz de inserir a 
+                                nossa sociedade moderna a um novo patamar 
+                                jamais visto antes. E meu maior objetivo é 
+                                contribuir notoriamente com essa evolução </p>
+
+                        </div>
+                        <div class="socialIcons">
+                            <a href=""><i class="icon-facebook"></i></i></a>
+                            <a href=""><i class="icon-twitter"></i></a>
+                            <a href=""><i class="icon-gmail"></i></a>
+                            <a href=""><i class="icon-github"></i></a>
+                            <a href=""><i class="icon-youtube"></i></a>
+                            <a href=""><i class="icon-instagram"></i></a>
+                            <a href=""><i class="icon-linkedin"></i></i></a>
+                        </div>
+                        <footer class="aboutFooter">
+                            <a href="" class="goContact">Contato</a>
+                            <a class="goFlip">Portifólio</a>
+                        </footer>
+                    </article>
+
+                    <!-- card back -->
+
+                    <div class="aboutMeContainer card-back">
+
+                        <header class="aboutHeader">
+                            <h2 class="animated">Curriculo</h2>
+                        </header>
+
+                        <div class="backContent-wrap">
+
+                            <div class="backContent animated">
+                                <p><b>Resumo: </b>  Sou desenvolvedor front-end e designer de interfaces. E um grande fanático por usabilidade e por experiências visuais únicas. Gosto de trabalhar com desenvolvimento Web pois ela abrange vários campos de conhecimentos. 
+                                    <br />
+                                    <br />
+                                    Isso permite que eu não seja um profissional que só interage apenas com códigos. Por isso além de estudar unicamente tecnologias ligadas à web. Também sou um grande estudante de Direito, Psicologia, Marketing e User Experience.
+                                </p>
+
+                                <p><b>Formação Acadêmica: </b> Cursando o penúltimo
+                                    semestre do curso Sistemas para Internet na
+                                    Universidade Municipal de São Caetano do
+                                    sul (USCS).
+                                </p>
+                                <p class="list">
+                                    <b>Conhecimentos: </b>
+                                    <br />
+                                    <br />
+                                    • HTML5, CSS3. JavaScript e jQuery<br />
+                                    • Linguagens para dados estruturados:  Microdata,
+                                    Microformats e RDFa.<br />
+                                    • WAI-ARIA para acessibilidade <br />
+                                    • CMS’s: Wordpress e Magento<br />
+                                    • UX/UI Design<br />
+                                    • Marketing e Publicidade digital<br />
+                                    • SEO, SEM e SMM<br />
+                                    • Scrum e Kanban para gerenciamento de projetos <br />
+                                    • Git para versionamento de projetos <br />
+                                    • Inglês técnico <br />
+                                </p>
+                            </div>
+                        </div>
+
+                        <footer class="aboutFooter">
+                            <a href="" class="goContact">Contato</a>
+                            <a class="goFlip">Voltar</a>
+                        </footer>
+                    </div>
+                </div>
+            </section>
         </div>
         <div class="col">
-            
+
+            <section id="aboutPage">
+
+                <div class="aboutMe">
+                    <article class="aboutMeContainer card-front">
+                        <header class="aboutHeader">
+                            <img class="headerProfile img-fluid" src="../plugins/img/post03.jpg" alt="header image" />
+                            <h1 class="fadeInLeft animated">Cisco</h1>
+                            <p class="fadeInLeft animated">UI/ UX Designer - Web Developer</p>
+                        </header>
+
+                        <div class="frontContent">
+                            <p>Tenho 21 anos, moro na Grande São Paulo e sou 
+                                apaixonado pela Web. Ela me cativa principalmente
+                                por sua grande colaboração com o desenvolvimento
+                                social. Ela é um novo mundo capaz de inserir a 
+                                nossa sociedade moderna a um novo patamar 
+                                jamais visto antes. E meu maior objetivo é 
+                                contribuir notoriamente com essa evolução </p>
+
+                        </div>
+                        <div class="socialIcons">
+                            <a href=""><i class="icon-facebook"></i></i></a>
+                            <a href=""><i class="icon-twitter"></i></a>
+                            <a href=""><i class="icon-gmail"></i></a>
+                            <a href=""><i class="icon-github"></i></a>
+                            <a href=""><i class="icon-youtube"></i></a>
+                            <a href=""><i class="icon-instagram"></i></a>
+                            <a href=""><i class="icon-linkedin"></i></i></a>
+                        </div>
+                        <footer class="aboutFooter">
+                            <a href="" class="goContact">Contato</a>
+                            <a class="goFlip">Portifólio</a>
+                        </footer>
+                    </article>
+
+                    <!-- card back -->
+
+                    <div class="aboutMeContainer card-back">
+
+                        <header class="aboutHeader">
+                            <h2 class="animated">Currículo</h2>
+                        </header>
+
+                        <div class="backContent-wrap">
+
+                            <div class="backContent animated">
+                                <p><b>Resumo: </b>  Sou desenvolvedor front-end e designer de interfaces. E um grande fanático por usabilidade e por experiências visuais únicas. Gosto de trabalhar com desenvolvimento Web pois ela abrange vários campos de conhecimentos. 
+                                    <br />
+                                    <br />
+                                    Isso permite que eu não seja um profissional que só interage apenas com códigos. Por isso além de estudar unicamente tecnologias ligadas à web. Também sou um grande estudante de Direito, Psicologia, Marketing e User Experience.
+                                </p>
+
+                                <p><b>Formação Acadêmica: </b> Cursando o penúltimo
+                                    semestre do curso Sistemas para Internet na
+                                    Universidade Municipal de São Caetano do
+                                    sul (USCS).
+                                </p>
+                                <p class="list">
+                                    <b>Conhecimentos: </b>
+                                    <br />
+                                    <br />
+                                    • HTML5, CSS3. JavaScript e jQuery<br />
+                                    • Linguagens para dados estruturados:  Microdata,
+                                    Microformats e RDFa.<br />
+                                    • WAI-ARIA para acessibilidade <br />
+                                    • CMS’s: Wordpress e Magento<br />
+                                    • UX/UI Design<br />
+                                    • Marketing e Publicidade digital<br />
+                                    • SEO, SEM e SMM<br />
+                                    • Scrum e Kanban para gerenciamento de projetos <br />
+                                    • Git para versionamento de projetos <br />
+                                    • Inglês técnico <br />
+                                </p>
+                            </div>
+                        </div>
+
+                        <footer class="aboutFooter">
+                            <a href="" class="goContact">Contato</a>
+                            <a class="goFlip">Voltar</a>
+                        </footer>
+                    </div>
+                </div>
+            </section> 
         </div>
-        
+
     </div>
+
+
+
+
+
+
 
 </div>
 
